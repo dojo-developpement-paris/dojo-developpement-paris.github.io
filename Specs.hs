b3d350f3215201c90224cfea08262c55ee310428
Michel Belleville <michel.belleville@gmail.com>
2016-01-29 18:40:27 +0100

    Testing inclusion of multiple numbers at a time

--- Specs.hs
+++ Specs.hs
@@ -6,7 +6,10 @@ allTheNumbers l | l == [0..10] = "1098765432"
 allTheNumbers (a:as) = show a ++ allTheNumbers as
 
 shouldContainAll :: String -> [String] -> Expectation
-shouldContainAll _ _ = True `shouldBe` True
+shouldContainAll s ss = mapM_ (shouldContain s) ss
+
+shouldHaveSize :: String -> Int -> Expectation
+shouldHaveSize s l = s `shouldSatisfy` (((==) l) . length)
 
 main = hspec $ do
   describe "all the numbers kata" $ do
@@ -17,16 +20,13 @@ main = hspec $ do
         allTheNumbers [1] `shouldBe` "1"
         allTheNumbers [2] `shouldBe` "2"
       it "shows a two-char string given two one-digit number in a interval" $ do
-        allTheNumbers [1..2] `shouldBe` "12"
-        allTheNumbers [2..3] `shouldBe` "23"
+        allTheNumbers [1..2] `shouldContainAll` (map show [1..2])
+        allTheNumbers [1..2] `shouldHaveSize` 2
+        allTheNumbers [2..3] `shouldContainAll` (map show [2..3])
+        allTheNumbers [2..3] `shouldHaveSize` 2
       it "shows a 3-char string given an interval of three numbers"  $ do
-        allTheNumbers [2..4] `shouldBe` "234"
+        allTheNumbers [2..4] `shouldContainAll` (map show [2..4])
+        allTheNumbers [2..4] `shouldHaveSize` 3
       it "shows all number from 0 to 10 without repeating 1 and 0" $ do
-        allTheNumbers [0..10] `shouldBe` "1098765432"
-        allTheNumbers [0..10] `shouldContain` "10"
-
-  describe "shouldContainAll" $ do
-    it "matches all substrings" $ do
-      "1234" `shouldContainAll` ["1", "2", "3", "4"]
-    it "doesn't match all substrings" $ do
-      ("1234" `shouldContainAll` ["5"]) `shouldBe` (expectationFailure "blabla")
+        allTheNumbers [0..10] `shouldContainAll` (map show [0..10])
+        allTheNumbers [0..10] `shouldHaveSize`   10
