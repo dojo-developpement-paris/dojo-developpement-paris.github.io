f34ff63859f786043584eab26fbbfc7ed0b4bf27
ToF- <cthibaut@octo.com>
2017-06-02 20:09:36 +0200

    Red and turned

--- 2017-06-02/Glut.hs
+++ 2017-06-02/Glut.hs
@@ -10,4 +10,14 @@ main = do
 display :: DisplayCallback
 display = do
     clear [ ColorBuffer ]
+    scale 0.7 0.7 (0.7::GLfloat)
+    rotate (45::GLfloat) $ Vector3 0 0 1
+    color red
+    renderPrimitive Quads $ mapM_ (\(x,y,z) -> vertex $ Vertex3 x y z) points
     flush
+
+red :: Color3 GLfloat
+red = Color3 255 0 0
+
+points :: [(GLfloat,GLfloat,GLfloat)]
+points = [(0,0,0), (0,1,0),(1,1,0),(1,0,0)]
