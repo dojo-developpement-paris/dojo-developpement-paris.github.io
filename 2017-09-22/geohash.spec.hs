19a900d182d9d1cff87c57d2b11d670bf486de89
bec <benoit.chiquet@gmail.com>
2017-09-22 21:08:49 +0200

    stringly typed application

--- 2017-09-22/geohash.spec.hs
+++ 2017-09-22/geohash.spec.hs
@@ -1,4 +1,7 @@
 import Test.Hspec
+import Control.Applicative ((<*>))
+import Data.Char
+
 
 main = hspec $ do
   describe "neighbours of a cell" $ do
@@ -11,18 +14,16 @@ main = hspec $ do
                                 ,"T",      "V"
                                 ,"Z", "0", "1"] 
 
-neighbours loc@"U" = [[(north . west) locBit], [north locBit], [(north.east) locBit]
-                      ,[west locBit],                           [east locBit]
-                      ,[(south . west) locBit],        "0", "1"] 
-    where [locBit] = loc
-neighbours [locBit] = map (\f -> (:[]) (f locBit)) 
+neighbours cell = map return $ map to $ 
                           [north . west, north, north.east
                           ,west,                east
-                          ,south . west, south, south . east] 
+                          ,south . west, south, south . east] <*> cell
+
+to numChar | numChar > 'Z' = chr (ord numChar - ord 'Z' + ord '0' - 1)
+to char = char
 
 north = pred . pred . pred . pred . pred . pred
 west = pred
 east = succ
 south = succ . succ . succ . succ . succ . succ
-
 
