713623b9fac4110d0b23ed7043252fdedbb38e59
ToF- <cthibaut@octo.com>
2018-03-30 20:52:22 +0200

    match with less than 40 games

--- 2018-03-30/EloChart.hs
+++ 2018-03-30/EloChart.hs
@@ -1,11 +1,13 @@
 module EloChart 
 where
 import Data.List (sortBy)
+import Data.Maybe
 
 
 type Chart = [(Player, Elo)]
 type Player = String
 type Elo    = Double
+type Result = Double
 
 base :: Elo
 base = 1500
@@ -20,4 +22,18 @@ newChart :: Chart
 newChart = []
 
 add :: Player -> Chart -> Chart 
-add p c = (p, base) : c
+add p c | lookup p c == Nothing =  (p, base) : c
+        | otherwise             =  c
+
+match :: Player -> Player -> Result -> Chart -> Chart 
+match barb abel result chart = update barb newEloBarb ((update abel newEloAbel) chart) 
+    where
+
+    eloBarb = fromJust (lookup barb chart)  
+    eloAbel = fromJust (lookup abel chart)
+    diff = eloBarb - eloAbel
+
+    newEloBarb = eloBarb + 40 * (  result - 1 / (1+ 10 ** (-diff / 400.0)))
+    newEloAbel = eloAbel + 40 * (1-result - 1 / (1+ 10 ** (-diff / 400.0)))
+    update :: Player -> Elo -> Chart -> Chart
+    update player elo chart = (player,elo) : filter (\(p',_) -> p' /= player) chart  
