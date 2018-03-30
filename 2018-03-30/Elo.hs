fcf349bfefce6a3102177594af26a9c312fd7390
ToF- <cthibaut@octo.com>
2018-03-30 20:04:44 +0200

    record a new player

--- 2018-03-30/Elo.hs
+++ 2018-03-30/Elo.hs
@@ -1,8 +1,27 @@
 import EloChart
+import System.Environment
 chartFileName = "players.txt"
 
+type Args = [String]
 
-main = do
+process :: Args -> IO ()
+process ["-P"] = do
     content <- readFile chartFileName 
     let chart = read content
     putStrLn (display chart)
+
+process ["-A",p] = do
+    content <- readFile chartFileName 
+    let chart = read content
+        chart' = add p chart 
+    putStrLn (display chart')
+    writeFile chartFileName (show chart') 
+
+process _ = do
+    putStrLn "usage : elo [-P | -A player]" 
+                      
+
+main :: IO ()
+main = do
+    args <- getArgs
+    process args
