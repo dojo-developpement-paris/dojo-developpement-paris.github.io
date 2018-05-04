0ec530d5f55b6ae05402822f3246420f437cacc1
FaustXVI <xavier.detant@gmail.com>
2018-05-04 20:50:59 +0200

    Having fun :)

--- 2018-07-06/luhn/src/main.rs
+++ 2018-07-06/luhn/src/main.rs
@@ -1,4 +1,14 @@
+extern crate luhn;
+
+use luhn::luhn::luhn;
+
 fn main() {
-    println!("Hello, world!");
+    let range = 10..1000000;
+    let nb_nb = range.clone().count();
+    let nb_luhn = range.filter(|&n| {
+        luhn(n)
+    }).count();
+    println!("{}", nb_luhn as f64 / nb_nb as f64);
+
 }
 
