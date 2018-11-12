8728a730b85bca55f1b2fef8b84dc1f4c9fc6bdb
Patrick GIRY <patrickgiry@gmail.com>
2018-11-12 19:55:32 +0100

    should shout HELLO JERRY! when given JERRY

--- 2018-11-12/Spec.hs
+++ 2018-11-12/Spec.hs
@@ -7,6 +7,9 @@ main = hspec $ do
             greet (Just "Mark")  `shouldBe`  "Hello, Mark."
         it "should say Hello my friend when no name given" $ do
             greet Nothing  `shouldBe` "Hello, my friend." 
+        it "should shout when given a name in uppercase" $ do
+            greet (Just "JERRY") `shouldBe` "HELLO JERRY!"
 
+greet (Just "JERRY") = "HELLO JERRY!"
 greet (Just name) = "Hello, " ++ name ++ "."
 greet Nothing     = greet (Just "my friend")
