a3bdab683c827f5ac69a98d8b605bde0ca8b37bc
Patrick GIRY <patrickgiry@gmail.com>
2018-11-12 19:46:43 +0100

    should say hello my friend when no name is given

--- 2018-11-12/Spec.hs
+++ 2018-11-12/Spec.hs
@@ -3,7 +3,11 @@ import Test.Hspec
 main = hspec $ do
     describe "greeting" $ do    
         it "should say Hello Bob when given Bob" $ do
-            greet "Bob"  `shouldBe`  "Hello, Bob."
-            greet "Mark"  `shouldBe`  "Hello, Mark."
+            greet (Just "Bob")  `shouldBe`  "Hello, Bob."
+            greet (Just "Mark")  `shouldBe`  "Hello, Mark."
+        it "should say Hello my friend when no name given" $ do
+            greet Nothing  `shouldBe` "Hello, my friend." 
 
-greet name = "Hello, " ++ name ++ "."
+
+greet (Just name) = "Hello, " ++ name ++ "."
+greet _           = "Hello, my friend."
