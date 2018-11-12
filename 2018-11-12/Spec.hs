981d8c944467b21ca4688d4be00fb4b8d5a9c485
Patrick GIRY <patrickgiry@gmail.com>
2018-11-12 20:06:29 +0100

    refactoring Maybe to List

--- 2018-11-12/Spec.hs
+++ 2018-11-12/Spec.hs
@@ -4,18 +4,19 @@ import Data.Char (isUpper)
 main = hspec $ do
     describe "greeting" $ do    
         it "should say Hello Bob when given Bob" $ do
-            greet (Just "Bob")  `shouldBe`  "Hello, Bob."
-            greet (Just "Mark")  `shouldBe`  "Hello, Mark."
+            greet ["Bob"]  `shouldBe`  "Hello, Bob."
+            greet ["Mark"]  `shouldBe`  "Hello, Mark."
         it "should say Hello my friend when no name given" $ do
-            greet Nothing  `shouldBe` "Hello, my friend." 
+            greet []  `shouldBe` "Hello, my friend." 
         it "should shout when given a name in uppercase" $ do
-            greet (Just "JERRY") `shouldBe` "HELLO JERRY!"
-            greet (Just "MARY") `shouldBe` "HELLO MARY!"
+            greet ["JERRY"] `shouldBe` "HELLO JERRY!"
+            greet ["MARY"] `shouldBe` "HELLO MARY!"
+        
 
 sayHello name = "Hello, " ++ name ++ "." 
 
 shoutHello name = "HELLO " ++ name ++ "!"
 
-greet (Just name) | all isUpper name  = shoutHello name
-greet (Just name) = sayHello name
-greet Nothing     = sayHello "my friend"
+greet [name] | all isUpper name  = shoutHello name
+greet [name] = sayHello name
+greet []     = sayHello "my friend"
