47acedf53416b2ffb19a4ef28c6ff7c0c6b6b094
Patrick GIRY <patrickgiry@gmail.com>
2018-11-19 19:50:04 +0100

    Une cellule qui est morte et qui n'a pas de voisin reste morte

--- Specs.hs
+++ Specs.hs
@@ -1,6 +1,11 @@
 import Test.Hspec
 
+data Liveness = Dead
+    deriving (Eq, Show)
+
+nextGeneration _ _ = Dead
+
 main = hspec $ do
     describe "game of life" $ do
-        it "should fail" $ do
-            True `shouldBe` True
+        it "Cell should die when no neighbors" $ do
+            nextGeneration Dead 0  `shouldBe` Dead 
