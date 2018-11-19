2f2450dc3264c2941def7b0faba91a454d16f8f4
Patrick GIRY <patrickgiry@gmail.com>
2018-11-19 19:56:17 +0100

    squash! Une cellule qui est morte et qui n'a pas de voisin reste morte Refacto change name

--- Specs.hs
+++ Specs.hs
@@ -3,9 +3,9 @@ import Test.Hspec
 data Liveness = Dead
     deriving (Eq, Show)
 
-nextGeneration _ _ = Dead
+cellStatus _ _ = Dead
 
 main = hspec $ do
     describe "game of life" $ do
         it "Cell should die when no neighbors" $ do
-            nextGeneration Dead 0  `shouldBe` Dead 
+            cellStatus Dead 0  `shouldBe` Dead 
