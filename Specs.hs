d41a3bb00f3b7b331743dd52a0edc91352cd5697
Patrick GIRY <patrickgiry@gmail.com>
2018-11-19 20:02:09 +0100

    Nouvelle règle pour la vie de la cellule

--- Specs.hs
+++ Specs.hs
@@ -1,11 +1,14 @@
 import Test.Hspec
 
-data Liveness = Dead
+data Liveness = Dead | Alive
     deriving (Eq, Show)
 
+cellStatus _ 3 = Alive 
 cellStatus _ _ = Dead
 
 main = hspec $ do
     describe "game of life" $ do
         it "Cell should die when no neighbors" $ do
             cellStatus Dead 0  `shouldBe` Dead 
+        it "Cell should live when 3 neighbors" $ do
+            cellStatus Dead 3  `shouldBe` Alive 
