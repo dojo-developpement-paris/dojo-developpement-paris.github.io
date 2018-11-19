02486d490eb8e7d9bf178b9f2b44437c2952f679
Patrick GIRY <patrickgiry@gmail.com>
2018-11-19 20:20:53 +0100

    Une cellule morte avec 2 voisins reste morte
    Une cellule vivante avec 3 voisins reste vivante

--- 2018-11-19/Specs.hs
+++ 2018-11-19/Specs.hs
@@ -12,8 +12,13 @@ main = hspec $ do
           describe "when dead should" $ do
              it "stay dead when no neighbors" $ do
                  cellStatus Dead 0  `shouldBe` Dead 
+             it "stay dead when 2 neighbors" $ do
+                 cellStatus Dead 2  `shouldBe` Dead 
              it "resurrect when 3 neighbors" $ do
                  cellStatus Dead 3  `shouldBe` Alive 
           describe "when alive should" $ do
              it "stay alive when 2 neighbors" $ do
                  cellStatus Alive 2  `shouldBe` Alive 
+             it "stay alive when 3 neighbors" $ do
+                 cellStatus Alive 3 `shouldBe` Alive 
+
