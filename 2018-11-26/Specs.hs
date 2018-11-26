97d46b499dff0310b71f3af65fa8bb1bb212cb03
Patrick GIRY <patrickgiry@gmail.com>
2018-11-26 20:17:46 +0100

    Reports a deposit

--- 2018-11-26/Specs.hs
+++ 2018-11-26/Specs.hs
@@ -2,14 +2,24 @@ import Test.Hspec
 import Control.Monad.Writer (writer, runWriter)
 import Bank
 
+
 out = \s -> writer ((),s)
 
 main = hspec $ do 
     describe "Bank" $ do
         it "outputs pretty report" $ do
-                let run = bank out
+                let run = bank [] out
                 (lines.snd) (runWriter run)  `shouldBe` 
                         ["Solde initial : 0"
                         ,"Date | Debit | Credit"
                         ,"Solde : 0"
                         ]
+        it "reports a deposit" $ do
+                let run = bank [Deposit "26/11/2018" 4807] out
+                (lines.snd) (runWriter run)  `shouldBe` 
+                        ["Solde initial : 0"
+                        ,"Date | Debit | Credit"
+                        , "26/11/2018 | | 4807"
+                        ,"Solde : 4807"
+                        ]
+
