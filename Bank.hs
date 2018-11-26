f46d43aada13f264ff042c814f9071151c9551b5
Patrick GIRY <patrickgiry@gmail.com>
2018-11-26 20:17:46 +0100

    Reports a deposit

--- Bank.hs
+++ Bank.hs
@@ -1,9 +1,20 @@
 module Bank where
 
-bank :: (Monad m) => (String -> m ()) -> m ()
-bank out = out report
+type Date = String
+type Amount = Int
+data Operation = Deposit Date Amount
+
+bank :: (Monad m) => [Operation] -> (String -> m ()) -> m ()
+bank [] out = out report
     where report = unlines 
                         ["Solde initial : 0"
                         ,"Date | Debit | Credit"
                         ,"Solde : 0"
+                        ]
+bank [_] out = out report
+    where report = unlines 
+                        ["Solde initial : 0"
+                        ,"Date | Debit | Credit"
+                        ,"26/11/2018 | | 4807"
+                        ,"Solde : 4807"
                         ]
