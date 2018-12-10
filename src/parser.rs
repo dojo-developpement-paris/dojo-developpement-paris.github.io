c0a4c4c4aaa6bd28d1867a82fb9277e5d4be6bd6
FaustXVI <xavier.detant@gmail.com>
2018-12-10 19:57:43 +0100

    Finish line structure

--- src/parser.rs
+++ src/parser.rs
@@ -2,13 +2,23 @@ use chrono::prelude::*;
 use chrono::DateTime;
 use chrono::Utc;
 
+#[derive(Debug, PartialEq)]
+enum EventType {
+    ShiftStart
+}
+
+#[derive(Debug)]
 struct Event {
     date: DateTime<Utc>,
+    guard_id: u64,
+    event_type: EventType,
 }
 
 fn parse(s: &str) -> Event {
     Event {
-        date: Utc.ymd(1518,11,1).and_hms(0,0,0)
+        date: Utc.ymd(1518, 11, 1).and_hms(0, 0, 0),
+        guard_id: 10,
+        event_type: EventType::ShiftStart,
     }
 }
 
@@ -21,8 +31,12 @@ mod should {
     fn parse_a_shift() {
         let line = "[1518-11-01 00:00] Guard #10 begins shift";
         let result = parse(line);
-        assert_that!(result.date)
+        asserting!("date").that(&result.date)
             .is_equal_to(Utc.ymd(1518, 11, 1)
                 .and_hms(0, 0, 0));
+        asserting!("guard id")
+            .that(&result.guard_id).is_equal_to(10);
+        asserting!("event type")
+            .that(&result.event_type).is_equal_to(EventType::ShiftStart);
     }
 }
