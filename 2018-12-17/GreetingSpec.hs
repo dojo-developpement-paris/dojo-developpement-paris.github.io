f171c9cfaaf662e0598bc4f979da682b7b971dc7
Patrick GIRY <patrickgiry@gmail.com>
2018-12-17 19:56:00 +0100

    should say hello my friend with no given name

--- 2018-12-17/GreetingSpec.hs
+++ 2018-12-17/GreetingSpec.hs
@@ -1,13 +1,19 @@
 import Test.Hspec
 
-greet :: String -> String
-greet n = "Hello, " ++ n ++ "."
+greet :: Maybe String -> String
+greet (Just n) = sayHello n 
+greet Nothing  = sayHello "my friend"
+
+sayHello :: String -> String
+sayHello n = "Hello, " ++ n ++ "."
 
 main = hspec $ do
     describe "Greet" $ do
         it "should say hello to the given name" $ do
-          greet "Bob"  `shouldBe` "Hello, Bob."
-          greet "Alice"  `shouldBe` "Hello, Alice."
+          greet (Just "Bob")  `shouldBe` "Hello, Bob."
+          greet (Just "Alice")  `shouldBe` "Hello, Alice."
+        it "should say hello my friend with no given name" $ do
+          greet Nothing  `shouldBe` "Hello, my friend."
 
         
 
