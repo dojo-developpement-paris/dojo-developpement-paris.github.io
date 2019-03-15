271f5cff3407149ff00cf8435b0b8d9138855d39
Patrick GIRY <patrickgiry@gmail.com>
2019-03-15 20:45:16 +0100

    done better

--- 2019-03-15/Diamond.hs
+++ 2019-03-15/Diamond.hs
@@ -8,7 +8,9 @@ diamond l = map mirror $ mirror r
         n = length ls
         m = n - 1
         ms = [0..m]
-        r = map (\i -> replicate (ms!!(m-i)) ' ' ++ [ls!!i] ++ replicate (ms!!i) ' ') ms
+        r = map (\i -> spaces (ms!!(m-i)) ++ [ls!!i] ++ spaces (ms!!i) ) ms
 
 mirror :: [a] -> [a] 
 mirror xs = xs ++ (tail (reverse xs)) 
+
+spaces x = replicate x ' ' 
