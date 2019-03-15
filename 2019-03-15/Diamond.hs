6abbd2a5a1de432450fdbad991e25b148b07b9a8
Patrick GIRY <patrickgiry@gmail.com>
2019-03-15 20:49:11 +0100

    done ord

--- 2019-03-15/Diamond.hs
+++ 2019-03-15/Diamond.hs
@@ -1,5 +1,6 @@
 module Diamond (diamond)
 where
+-- import Data.Char (ord)
 
 diamond :: Char -> [String]
 diamond l = map mirror $ mirror r
@@ -9,6 +10,7 @@ diamond l = map mirror $ mirror r
         m = n - 1
         ms = [0..m]
         r = map (\i -> spaces (ms!!(m-i)) ++ [ls!!i] ++ spaces (ms!!i) ) ms
+        -- r = map (\c -> spaces (ord l - ord c) ++ [c] ++ spaces (ord c - ord 'A') ) ls
 
 mirror :: [a] -> [a] 
 mirror xs = xs ++ (tail (reverse xs)) 
