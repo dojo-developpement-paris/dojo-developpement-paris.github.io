f9d9f8f93c27770a20dd42f578347772c0765fee
Patrick GIRY <patrickgiry@gmail.com>
2019-03-15 20:43:12 +0100

    simplify

--- 2019-03-15/Diamond.hs
+++ 2019-03-15/Diamond.hs
@@ -2,23 +2,6 @@ module Diamond (diamond)
 where
 
 diamond :: Char -> [String]
-diamond 'A' = map mirror $ mirror [a]
-    where
-        ls = ['A'..'A']
-        n = length ls
-        m = n - 1
-        ms = [0..m]
-        a = replicate m ' ' ++ "A" ++ replicate m ' '
-
-diamond 'B' = map mirror $ mirror [a, b]
-    where
-        ls = ['A'..'B']
-        n = length ls
-        m = n - 1
-        ms = [0..m]
-        a = replicate (ms!!1) ' ' ++ [ls!!0] ++ replicate (ms!!0) ' ' 
-        b = replicate (ms!!0) ' ' ++ [ls!!1] ++ replicate (ms!!1) ' '
-
 diamond l = map mirror $ mirror r
     where
         ls = ['A'..l]
