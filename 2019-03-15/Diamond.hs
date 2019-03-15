f008a6a1130a7b78f6004488f9254569c8eae04f
Patrick GIRY <patrickgiry@gmail.com>
2019-03-15 20:14:38 +0100

    refactor a b

--- 2019-03-15/Diamond.hs
+++ 2019-03-15/Diamond.hs
@@ -3,7 +3,10 @@ where
 
 diamond :: Char -> [String]
 diamond 'A' = map mirror $ mirror ["A"]
-diamond 'B' = map mirror $ mirror [" A","B "]
+diamond 'B' = map mirror $ mirror [" A","B "] 
+    where
+    a = " A"
+    b = "B "
 diamond 'C' = map mirror $ mirror ["  A"," B ","C  "]
 
 mirror :: [a] -> [a] 
