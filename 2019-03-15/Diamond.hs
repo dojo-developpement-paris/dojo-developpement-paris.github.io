0da5d47d85436f630978ef7a2587b428904cfe37
Patrick GIRY <patrickgiry@gmail.com>
2019-03-15 20:17:34 +0100

    replicate 1 et 0

--- 2019-03-15/Diamond.hs
+++ 2019-03-15/Diamond.hs
@@ -5,8 +5,8 @@ diamond :: Char -> [String]
 diamond 'A' = map mirror $ mirror ["A"]
 diamond 'B' = map mirror $ mirror [a, b]
     where
-        a = replicate 1 ' ' ++ "A"
-        b = "B "
+        a = replicate 1 ' ' ++ "A" ++ replicate 0 ' ' 
+        b = replicate 0 ' ' ++ "B" ++ replicate 1 ' '
 diamond 'C' = map mirror $ mirror [a, b, c]
     where
         a = "  A"
