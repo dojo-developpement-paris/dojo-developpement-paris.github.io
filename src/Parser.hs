60e64d175d12d0a7df7ce2248a68678190ee5fb6
Benoît CHIQUET <bchiquet@vente-privee.com>
2019-11-30 18:31:21 +0100

    remove all the tree
    * In the process of removing the tree, `Apply` became less typed...
    * We would like to regain typing in order to constrain the parsing.
    * We have now completely removed the tree, `Token` is self-carried

--- src/Parser.hs
+++ src/Parser.hs
@@ -8,12 +8,9 @@ type Value = Integer
 type Operation = Char
 type Parser a = String -> [(a,String)]
 
-data Tree a = Nil 
-            | Node a (Tree a)
-
 data Token = V Value
            | Lambda Operation
-           | Apply Operation (Tree Token)
+           | Apply Token Token
 
 normal :: String -> String
 normal s = case (map fst . filter success . parse) s of
@@ -29,17 +26,17 @@ debug s = case parse s of
 success :: (a,String) -> Bool
 success = null . snd
 
-parse :: Parser (Tree Token)
+parse :: Parser Token
 parse = expression
 
-expression :: Parser (Tree Token)
+expression :: Parser Token
 expression = number 
            <|> (unary <&> expression)
            <|> (binary <&> expression <&> expression)
 
-number = spaces (leaf valueParser) 
-unary  = spaces (leaf unaryParser)
-binary = spaces (leaf binaryParser)
+number = spaces valueParser 
+unary  = spaces unaryParser
+binary = spaces binaryParser
            
 
 valueParser :: Parser Token
@@ -64,9 +61,6 @@ binaryParser (c:s) = case lookup c binaries of
     Just f -> [(Lambda c,s)]
 binaryParser _ = []
 
-leaf :: Parser a -> Parser (Tree a) 
-leaf p = fmap (\(a,s) -> (Node a Nil, s)) . p
-
 spaces :: Parser a -> Parser a
 spaces p (' ':rest) = spaces p rest
 spaces p s       = p s
@@ -76,21 +70,18 @@ infixl 2 <|>
 (p <|> q) s = p s <> q s
 
 infixl 3 <&>
-(<&>) :: Parser (Tree a) -> Parser (Tree a) -> Parser (Tree a)
+(<&>) :: Parser Token -> Parser Token -> Parser Token
 (p <&> q) s = case p s of
     [] -> []
     rs -> [(grow a b, s'')
           | (a,s') <- rs
           , (b,s'') <- q s']
     where
-    grow :: Tree a -> Tree a -> Tree a
-    grow Nil t = t
-    grow (Node a Nil) u = Node a u
-    grow (Node a t) u = Node (Apply a t) u
+    grow :: Token -> Token -> Token
+    grow (V a) _ = V a
+    grow lam t = Apply lam t
 
-eval :: Tree Token -> Value
-eval (Node (V n) _) = n
-eval (Node (Lambda c) t) = eval (Node (Apply c t) Nil)
---eval (Node (Lambda c) t u) = eval (Node (Apply c t) u)
-eval (Node (Apply c t) Nil) = let f = fromJust (lookup c unaries) in f (eval t)
-eval (Node (Apply c t) u) = let f = fromJust (lookup c binaries) in f (eval t) (eval u)
+eval :: Token -> Value
+eval (V n) = n
+eval (Apply (Lambda c) t) = let f = fromJust (lookup c unaries) in f (eval t)
+eval (Apply (Apply (Lambda c) t) u) = let f = fromJust (lookup c binaries) in f (eval t) (eval u)
