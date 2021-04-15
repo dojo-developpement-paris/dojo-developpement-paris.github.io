8ffa76b895d97ab83c625fc2259ab0513536d880
bec <benoit.chiquet@gmail.com>
2021-04-15 21:04:43 +0200

    le code commis ce soir là

--- 2021-04-15/src/Lib.hs
+++ 2021-04-15/src/Lib.hs
@@ -1,6 +1,30 @@
 module Lib
     ( someFunc
+    , compute
+    , function
     ) where
 
+import Data.Function
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = function getLine putStrLn
+
+
+function :: Monad m => m String -> (String -> m ()) -> m ()
+function inputFn outputFn = do
+  outputFn "Entrez un prix" 
+  input <- inputFn
+  case input of
+    "" -> return () 
+    price -> outputFn (compute price)
+
+
+compute :: String -> String
+compute s = show partieEntiere <> "." <> take 2 (show centimes)
+  where
+    partieEntiere = (read s :: Double)
+                  & truncate
+    centimes      = (read s :: Double)
+                  & (\d -> d * 100)
+                  & truncate
+                  & (`mod` 100) 
