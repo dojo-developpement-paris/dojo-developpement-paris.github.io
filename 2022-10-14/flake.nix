128b3391f07e973b8c3940812dc8a43f6c1dbff3
pinage404 <pinage404@gmail.com>
2022-10-14 21:18:02 +0200

    le code de ce soir

---
+++ 2022-10-14/flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "TypeScript with formatting and test on NodeJS";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages = [
+            pkgs.nodejs-18_x
+          ];
+
+          shellHook = ''
+            npm install
+          '';
+        };
+      });
+}
