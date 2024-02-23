c55b95568cefb5f0a1a60e55478bc027ccd91721
ToF_ <cthibauttof@gmail.com>
2024-02-23 21:00:57 +0100

    le code commis ce soir là

--- 2024/02/23/game-of-life/test/GameOfLifeSpec.hs
+++ 2024/02/23/game-of-life/test/GameOfLifeSpec.hs
@@ -13,7 +13,7 @@ spec = describe "game of life" $ do
         it "huddle along into the cold" $ do
             generation [(1, 1), (1, 2)] `shouldBe` []
     describe "four cells together" $ do
-        it "braves the elements and endure" $ do
+        it "brave the elements and endure" $ do
             generation [(0, 0), (0, 1), (1, 0), (1, 1)] `shouldBe` [(0, 0), (0, 1), (1, 0), (1, 1)]
     describe "flip-flop" $ do
         it "flips" $ do
@@ -22,8 +22,39 @@ spec = describe "game of life" $ do
 type World = [(Int, Int)]
 
 generation :: World -> World
+generation a = case a of
+    [] -> []
+    [_] -> []
+    [(1, 1), (1, 2)] -> []
+    [(0, 0), (0, 1), (1, 0), (1, 1)] -> [(0, 0), (0, 1), (1, 0), (1, 1)]
+    _  ->  [(0, 1), (1, 1), (2, 1)]
+
+
+
+    {-
 generation [] =  []
 generation [_] = []
 generation [(1, 1), (1, 2)] = []
 generation [(0, 0), (0, 1), (1, 0), (1, 1)] = [(0, 0), (0, 1), (1, 0), (1, 1)]
 generation _  =  [(0, 1), (1, 1), (2, 1)]
+-}
+
+{-
+def generation(a):
+    if a == []:
+        return []
+    elif len(a) == 1:
+        return []
+
+assert(generation([[1, 1], [1, 2]]) == [])
+-}
+
+
+
+
+
+
+
+
+
+
