25ad9ad3afd4d0e58d5ff9a775f062f3bf0ad1c2
pinage404 <pinage404@gmail.com>
2026-04-03 21:01:38 +0200

    le code de ce soir

--- 2026/04/03/2026/04/03/puissance4/src/main.rs
+++ 2026/04/03/2026/04/03/puissance4/src/main.rs
@@ -1,27 +1,56 @@
-use std::env;
+use std::io;
 
-fn main() {
-    let name = env::args().nth(1);
+const YELLOW_PAWN: &str = "X";
+const RED_PAWN: &str = "O";
+const EMPTY: &str = "-";
 
-    println!("{}", hello(name.as_deref()));
-}
+fn main() -> io::Result<()> {
+    let mut color = "Yellow";
+    let mut grid: [[&str; 7]; 6] = [
+        [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+        [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+        [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+        [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+        [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+        [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+    ];
+    loop {
+        println!("1 2 3 4 5 6 7");
+        println!("{}", grid.map(|line| { line.join(" ") }).join("\n"));
+        println!("{color}, choose a column:");
 
-fn hello(name: Option<&str>) -> String {
-    format!("Hello {}", name.unwrap_or("world"))
-}
+        let mut entry = String::new();
+        io::stdin()
+            .read_line(&mut entry)
+            .expect("Failed to read line");
 
-#[cfg(test)]
-mod test {
-    use super::*;
-    use speculoos::*;
-
-    #[test]
-    fn hello_world() {
-        assert_that(&hello(None)).is_equal_to("Hello world".to_string())
+        match entry.trim().parse::<usize>() {
+            Ok(column) if column > 0 && column < 8 => {
+                println!("Wow, nice");
+                if grid[5][column - 1] == EMPTY {
+                    let height = first_empty(grid, column);
+                    grid[height][column - 1] = if color == "Yellow" {
+                        YELLOW_PAWN
+                    } else {
+                        RED_PAWN
+                    };
+                } else {
+                    grid[4][column - 1] = if color == "Yellow" {
+                        YELLOW_PAWN
+                    } else {
+                        RED_PAWN
+                    };
+                }
+                color = if color == "Yellow" { "Red" } else { "Yellow" };
+            }
+            _ => {
+                println!("Nan nan reviens demain");
+            }
+        }
     }
-
-    #[test]
-    fn hello_foo() {
-        assert_that(&hello(Some("foo"))).is_equal_to("Hello foo".to_string())
-    }
+}
+fn first_empty(grid: [[&str; 7]; 6], column: usize) -> usize {
+    // TODO: ask Claude to finish for us
+    //grid.iter().find_index(|line| line[column - 1] != EMPTY)
+    5
 }
