060530b326853852d890b8afb7eff7af9cdb2811
pinage404 <pinage404@gmail.com>
2026-08-01 19:08:38 +0200

    suppression de fichiers générés
    ça ne changera pas la taille du repo, mais au moins c'est plus clean

--- 2018/07/26/factorial/target/debug/deps/factorial-a247a9b559e425dc.d
+++
@@ -1,5 +0,0 @@
-/Users/Christophe/dev/dojo/dojo-developpement-paris.github.io/factorial/target/debug/deps/factorial-a247a9b559e425dc: src/lib.rs
-
-/Users/Christophe/dev/dojo/dojo-developpement-paris.github.io/factorial/target/debug/deps/factorial-a247a9b559e425dc.d: src/lib.rs
-
-src/lib.rs:
--- 2018/07/26/factorial/target/debug/libfactorial.d
+++
@@ -1,1 +0,0 @@
-/Users/Christophe/dev/dojo/dojo-developpement-paris.github.io/factorial/target/debug/libfactorial.rlib: /Users/Christophe/dev/dojo/dojo-developpement-paris.github.io/factorial/src/lib.rs
